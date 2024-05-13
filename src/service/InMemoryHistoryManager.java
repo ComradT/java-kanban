fb932147b449f93afdbaac53d0d10a85eb254cb5
Anatolii Work <ck.lion@bk.ru>
2024-05-13 17:22:34 +0500

    6 спринт, fixed.

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -13,7 +13,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     private Node first;
     private Node last;
 
-    private static class Node {
+    private static final class Node {
         Node next;
         Node prev;
         Task task;
